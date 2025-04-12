99ab899e8b04def3c99e4ba53a43fa41053c4a75
hw <hyeongu0305@gmail.com>
2025-04-12 16:17:59 +0900

    메뉴 반응형 확장 버튼 동작 추가

---
+++ index.js
@@ -0,0 +1,8 @@
+let menuToggle = document.getElementById('menu-toggle')
+let navLinks = document.getElementById('navLinks')
+
+// addEventListener : 추가해라 이벤트를 듣는놈을
+menuToggle.addEventListener('click', function () {
+    navLinks.classList.toggle('active')
+})
+
